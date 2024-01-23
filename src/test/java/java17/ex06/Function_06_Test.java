e329f81b40d044c2b61c2d0ce8c079f2253196d5
Driss <drissgazzaoui1986@hotmail.com>
2024-01-23 12:56:49 +0100

    tp function fini

--- src/test/java/java17/ex06/Function_06_Test.java
+++ src/test/java/java17/ex06/Function_06_Test.java
@@ -18,7 +18,9 @@ public class Function_06_Test {
     // TODO la méthode retourne une chaîne de caractères de la forme [age=<AGE>] (exemple : [age=12])
     String formatAge(Supplier<Person> supplier) {
         // TODO
-        return null;
+    	Person p = supplier.get();
+    	
+    	return "[age=" + p.getAge() +"]";
     }
     // end::formatAge[]
 
@@ -26,7 +28,8 @@ public class Function_06_Test {
     @Test
     public void test_supplier_formatAge() throws Exception {
         // TODO compléter le test unitaire pour qu'il soit passant
-        String result = formatAge(null);
+    	Supplier<Person> pe = ()->new Person("Achraf","Hakimi",35, "pass");
+        String result = formatAge(pe);
 
         assert result.equals("[age=35]");
     }
